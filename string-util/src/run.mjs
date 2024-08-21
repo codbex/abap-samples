d3d17c2daa5225a7d385905201e76922e4a56416
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2024-08-21 16:44:47 +0300

    remove Kronos db init logic to decouple the project

--- string-util/src/run.mjs
+++ string-util/src/run.mjs
@@ -1,13 +1,7 @@
-import { DatabaseInitializer } from "sdk/abap/database";
 import { initializeABAP } from '../dist/abap/init.mjs';
-
 import { zcl_abap_app } from '../dist/abap/zcl_abap_app.clas.mjs';
 
-async function initialize() {
-    DatabaseInitializer.initDefaultDataSource();
-    await initializeABAP();
-}
 
-await initialize();
+await initializeABAP();
 
 await zcl_abap_app.run("Ivan");
