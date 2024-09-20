5728b6323f75d50d86a33a7c2e6a89d39731b15a
pavelbaltiyskibix <pavel.baltiyski@bix-consulting.bg>
2024-09-20 18:16:29 +0300

    Update concatenate-dto.ts

--- snowflake-api-project/api/concatenate-dto.ts
+++ snowflake-api-project/api/concatenate-dto.ts
@@ -1,6 +1,8 @@
 export interface ConcatenateParamsDTO {
     readonly param1: string;
     readonly param2: string;
+    readonly param3: string;
+    readonly param4: string;
 }
 
 export interface ConcatenateResultDTO {
